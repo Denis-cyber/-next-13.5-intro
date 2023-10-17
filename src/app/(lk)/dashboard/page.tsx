4699601eabf602acfac081c22c7095bf7828f84f
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-10-17 18:50:54 +0300

    additional

--- src/app/(lk)/dashboard/page.tsx
+++ src/app/(lk)/dashboard/page.tsx
@@ -1,5 +1,12 @@
-function DashboardPage() {
-  return <div>Dashboard</div>;
-}
+// SSR | SSG | ISR
 
-export default DashboardPage;
+const fetchData = () =>
+  fetch("https://jsonplaceholder.typicode.com/todos/1", {
+    next: { revalidate: 10 },
+  }).then((response) => response.json());
+
+export default async function DashboardPage() {
+  const data = await fetchData();
+
+  return <div>{data?.title}</div>;
+}
